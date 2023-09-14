9c993b300210f3ec97d15f0f0bd9f17cb34c779d
Alexander Derritt <aderritt6158@gmail.com>
2023-09-13 23:53:04 -0500

    webpack setup

---
+++ webpack.config.ts
@@ -0,0 +1,69 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './client/index.tsx',
+  mode: process.env.NODE_ENV,
+  resolve: {
+    extensions: ['.tsx', '.ts', '.jsx', '.js'],
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+
+  module: {
+    rules: [
+      {
+        test: /.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        use: ['ts-loader'],
+      },
+      {
+        test: /.(css|scss)$/,
+        exclude: [/node_modules/],
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './client/index.html',
+    }),
+  ],
+  devServer: {
+    host: '0.0.0.0',
+    port: 8080,
+    hot: true,
+    liveReload: true,
+    historyApiFallback: true,
+    static: {
+      publicPat: '/build',
+      directory: path.join(__dirname, './build'),
+    },
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        secure: false,
+      },
+    },
+  },
+};
